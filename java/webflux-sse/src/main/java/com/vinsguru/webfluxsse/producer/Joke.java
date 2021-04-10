3372545abe5ec977b47688a9721e8fb3cee97504
wieslawbaranowski <wieslaw.baranowski@intive.com>
2021-04-10 04:14:20 +0200

    webflux

--- java/webflux-sse/src/main/java/com/vinsguru/webfluxsse/producer/Joke.java
+++ java/webflux-sse/src/main/java/com/vinsguru/webfluxsse/producer/Joke.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 import java.io.Serializable;
 
-@Data
+//@Data
 public class Joke implements Serializable {
 
     private static final String JOKE_FORMAT = "Q: %s \nA: %s";
@@ -12,9 +12,56 @@ public class Joke implements Serializable {
     private String setup;
     private String punchline;
 
+    public Joke() {
+    }
+
+    public Joke(final String setup, final String punchline) {
+        this.setup = setup;
+        this.punchline = punchline;
+    }
+
+    public String getSetup() {
+        return setup;
+    }
+
+    public void setSetup(final String setup) {
+        this.setup = setup;
+    }
+
+    public String getPunchline() {
+        return punchline;
+    }
+
+    public void setPunchline(final String punchline) {
+        this.punchline = punchline;
+    }
+
     @Override
     public String toString() {
         return String.format(JOKE_FORMAT, this.setup, this.punchline);
     }
 
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Joke joke = (Joke) o;
+
+        if (setup != null ? !setup.equals(joke.setup) : joke.setup != null) {
+            return false;
+        }
+        return punchline != null ? punchline.equals(joke.punchline) : joke.punchline == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = setup != null ? setup.hashCode() : 0;
+        result = 31 * result + (punchline != null ? punchline.hashCode() : 0);
+        return result;
+    }
 }
--- java/webflux-sse/src/main/java/com/vinsguru/webfluxsse/producer/JokePublisher.java
+++ java/webflux-sse/src/main/java/com/vinsguru/webfluxsse/producer/JokePublisher.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Sinks;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Service
 public class JokePublisher {
 
@@ -15,13 +18,18 @@ public class JokePublisher {
     @Autowired
     private Sinks.Many<Joke> sink;
 
-    @Scheduled(fixedRate = 3000)
+    @Scheduled(fixedRate = 3000000)
     public void publish(){
         this.webClient
                 .get()
                 .retrieve()
                 .bodyToMono(Joke.class)
-                .subscribe(this.sink::tryEmitNext);
+                .subscribe(joke-> {
+                    LocalDateTime dateTime = LocalDateTime.now();
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+                    System.out.println(dateTime.format(formatter)+"::"+ joke.toString());
+                    this.sink.tryEmitNext(joke);
+                });
     }
 
 }
